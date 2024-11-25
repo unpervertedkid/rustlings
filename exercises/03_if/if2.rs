e9024d37972f0e1b68a9f29676188f772ccf19e0
Brian Silah <unpervertedkid@gmail.com>
2024-11-25 06:04:27 +0300

    feat: 03 - if statements

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,9 +1,10 @@
-// TODO: Fix the compiler error on this function.
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
         "Yummy!"
+    } else if food == "potato" {
+       "I guess I can eat that." 
     } else {
-        1
+        "No thanks!"
     }
 }
 
